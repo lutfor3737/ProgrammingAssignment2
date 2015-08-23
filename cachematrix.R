16db83b854f93d5692d2078659fd9949333b7384
lutfor3737 <mdlutforrahman@csebuet.org>
2015-08-22 23:45:59 -0500

    calculate inverse matrix using lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  # mcache is the variable where to store inverse matrix results
+  mcache <- NULL
+  # setter function
+  set <- function(y) {
+    x <<- y
+    mcache <<- NULL
+  }
+  # Getter function
+  get <- function() x
+  # Inverse setter
+  setInverse <- function(inverse) mcache <<- inverse
+  # inverse Getter
+  getInverse <- function() mcache
+  # returns list with 4 member functions(set,get,setInverse,getInverse)
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
-
-## Write a short comment describing this function
-
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get inverse matrix from object x into mcache variable
+  mcache <- x$getInv()
+  # checking if already calculated, if already calculated then return with earlier results
+  if(!is.null(mcache)) { 
+    message("getting cached data")
+    return(mcache) 
+  }
+  # get object x into data variable
+  data <- x$get() 
+  # solve it
+  mcache <- solve(data)
+  # set inverted martix in cache
+  x$setInv(mcache) 
+  ## Return a matrix that is the inverse of 'x'
+  mcache 
+       
 }
